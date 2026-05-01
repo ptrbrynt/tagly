b870f861f40d7065179b4431098ee1e6cddebb34
Peter Bryant <pb@ptrbrynt.com>
2026-05-01 17:38:51 +0100

    Test database opening

---
+++ test/db/database_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import '../helpers/test_db.dart';
+
+void main() {
+  group('database', () {
+    test('opens successfully', () async {
+      final db = await openTestDb();
+
+      assert(db.isOpen, isTrue);
+    });
+  });
+}
---
+++ test/helpers/test_db.dart
@@ -0,0 +1,14 @@
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:tagly/db/database.dart';
+
+bool _ffiInitialized = false;
+
+Future<Database> openTestDb() async {
+  if (!_ffiInitialized) {
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+    _ffiInitialized = true;
+  }
+  final db = await openTaglyDatabase(path: inMemoryDatabasePath);
+  return db;
+}
